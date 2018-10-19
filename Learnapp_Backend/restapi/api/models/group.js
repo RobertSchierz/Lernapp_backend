38ea4b7b87e6d87fa58c51f2bf34cd0369f535b4
Robert Schierz <Robert.Schierz@hotmail.de>
2018-10-19 18:55:56 +0200

    Group erweitert
    Groupfunktionen:
    * PostMember
    * DeleteMember

--- Learnapp_Backend/restapi/api/models/group.js
+++ Learnapp_Backend/restapi/api/models/group.js
@@ -4,6 +4,13 @@ const groupSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     creator: {type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true},
     name: {type: String, required: true},
+    members: [{
+        role: {type: String, default: "Member"},
+        member: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }       
+    }] 
     
 });
 
--- Learnapp_Backend/restapi/api/routes/groups.js
+++ Learnapp_Backend/restapi/api/routes/groups.js
@@ -7,7 +7,7 @@ const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
     Group.find()
-        .select('_id name creator')
+        .select('_id name creator members')
         //.populate('creator')
         .exec()
         .then(docs => {
@@ -18,6 +18,7 @@ router.get('/', (req, res, next) => {
                         _id: doc._id,
                         creator: doc.creator,
                         name: doc.name,
+                        members: doc.members,
                         request: {
                             type: 'GET',
                             url: 'http://learnapp.enif.uberspace.de/restapi/groups/' + doc._id
@@ -43,12 +44,13 @@ router.post("/", (req, res, next) => {
                     message: 'User not found in the Database'
                 });
             }
-          
+
 
             const group = new Group({
                 _id: mongoose.Types.ObjectId(),
                 creator: req.body.creator,
-                name: req.body.name
+                name: req.body.name,
+                members: req.body.members
             });
 
             return group.save();
@@ -61,7 +63,8 @@ router.post("/", (req, res, next) => {
                 createdGroups: {
                     _id: result._id,
                     creator: result.creator,
-                    name: result.name
+                    name: result.name,
+                    members: result.members
                 },
                 request: {
                     type: 'GET',
@@ -82,52 +85,50 @@ router.post("/", (req, res, next) => {
 
 router.get('/:groupId', (req, res, next) => {
     Group.findById(req.params.groupId)
-    .exec()
-    .then(group => {
-        if(!group){
-            return res.status(404).json({
-                message: "Group not found: " + req.params.groupId
-            });
-        }
-        res.status(200).json({
-            group: group,
-            request: {
-                type: "GET",
-                url: "http://learnapp.enif.uberspace.de/restapi/groups"
+        .exec()
+        .then(group => {
+            if (!group) {
+                return res.status(404).json({
+                    message: "Group not found: " + req.params.groupId
+                });
             }
+            res.status(200).json({
+                group: group,
+                request: {
+                    type: "GET",
+                    url: "http://learnapp.enif.uberspace.de/restapi/groups"
+                }
+            })
         })
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
         });
-    });
-   
+
 
 });
 
 
-router.patch('/:groupId', (req, res, next) => {
-    const id = req.params.userId;
-    const updateOperations = {};
-    for (const ops of req.body) {
-        updateOperations[ops.propName] = ops.value;
-    }
-    Group.update({
-            _id: id
-        }, {
-            $set: updateOperations
+
+
+router.delete('/:groupId', (req, res, next) => {
+    Group.remove({
+            _id: req.params.groupId
         })
         .exec()
         .then(result => {
-            console.log(result);
-            console.log("Test: " + newName);
             res.status(200).json({
-                message: 'Group updated',
+                message: 'Group deleted',
                 request: {
-                    message: 'The possible Request to get the specific group:',
-                    type: 'GET',
-                    url: 'http://learnapp.enif.uberspace.de/restapi/users/' + id
+                    message: 'The possible Request to POST new Group:',
+                    type: 'POST',
+                    url: 'http://learnapp.enif.uberspace.de/restapi/groups/',
+                    body: {
+                        groupId: 'ID',
+                        name: 'String',
+                        members: '[{Members}]'
+                    }
                 }
             });
         })
@@ -137,29 +138,153 @@ router.patch('/:groupId', (req, res, next) => {
                 error: err
             })
         });
-
 });
 
-router.delete('/:groupId', (req, res, next) => {
-    Group.remove({_id: req.params.groupId})
-    .exec()
-    .then(result => {
-        res.status(200).json({
-            message: 'Group deleted',
-            request: {
-                message: 'The possible Request to POST new Group:',
-                type: 'POST',
-                url: 'http://learnapp.enif.uberspace.de/restapi/groups/',
-                body: { groupId: 'ID', name: 'String'}
+//Funktionen für Member:
+router.patch('/:groupId', (req, res, next) => {
+
+    Group.findById(req.params.groupId)
+        .exec()
+        .then(group => {
+            if (!group) {
+                return res.status(404).json({
+                    message: "Group not found: " + req.params.groupId
+                });
             }
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
+
+            const searchedMemberId = req.body.memberId;
+
+            // Patchcases for Groupmembers
+            switch (req.body.method) {
+                case "deleteMember":
+
+                    var checkMemberExists = false;
+                    for (var i = 0; i < group.members.length; i++) {
+                        if (group.members[i].member == searchedMemberId) {
+                            checkMemberExists = true;
+                        }
+                    }
+
+                    if (checkMemberExists) {
+
+                        //If Group and member exists then delete it
+                        Group.updateOne({
+                                _id: req.params.groupId
+                            }, {
+                                $pull: {
+                                    members: {
+                                        member: searchedMemberId
+                                    }
+                                }
+                            }).exec().then(result => {
+                                console.log(result);
+                                res.status(200).json({
+                                    message: 'Groupmember deleted: ' + searchedMemberId,
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                })
+                            });;
+
+
+
+                    } else {
+                        return res.status(404).json({
+                            message: "Groupmember not found: " + searchedMemberId
+                        });
+                    }
+
+                    break;
+
+                case "postMember":
+
+                    User.findById(req.body.memberId)
+                        .then(user => {
+                            if (!user) {
+                                return res.status(404).json({
+                                    message: 'User not found in the Database'
+                                });
+                            }
+
+                            var checkMemberExists = false;
+                            for (var i = 0; i < group.members.length; i++) {
+                                if (group.members[i].member == searchedMemberId) {
+                                    checkMemberExists = true;
+                                }
+                            }  
+
+                            if(!checkMemberExists){
+                               
+                                Group.updateOne({
+                                    _id: req.params.groupId
+                                }, {
+                                    $push: {
+                                        members: 
+                                            {
+                                                "role": "Member",
+                                                "member": searchedMemberId
+                                            }
+                                        }
+                                    }
+                                ).exec().then(result => {
+                                    console.log(result);
+                                    res.status(200).json({
+                                        message: 'Groupmember added: ' + searchedMemberId,
+                                    });
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                    res.status(500).json({
+                                        error: err
+                                    })
+                                });;  
+
+                            }else{
+                                return res.status(406).json({
+                                    message: "Groupmember allready in Group!: " + searchedMemberId
+                                });
+                            }
+
+
+
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+                            })
+                        });
+
+
+                    break;
+
+                default:
+                    break;
+            }
+
         })
-    });
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
+
 
 module.exports = router;
